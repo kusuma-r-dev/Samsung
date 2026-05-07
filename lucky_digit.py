7ccdd41845c5609eb0da9efb176ccdc2e391e9d9
kusumar004-debug <kusuma.r004@gmail.com>
2026-05-07 18:08:55 +0530

    Updated python file

--- lucky_digit.py
+++ lucky_digit.py
@@ -1,1 +1,19 @@
+inp_n = int(input("Enter a number to find your lucky digit : "))
+'''sum = 0
+for i in inp_n :
+    sum += i
+final_sum = 0
+for i in sum :
+    final_sum += i
+print(final_sum)'''
+print(f'Number you inpupt is {inp_n}')
+sum_of_digits = 0
+while inp_n != 0 :
+    rem = inp_n % 10
+    inp_n //= 10
+    sum_of_digits += rem
+    if sum_of_digits > 9 and inp_n == 0 :
+        inp_n = sum_of_digits
+        sum_of_digits = 0
 
+print("Your lucky digit is :",sum_of_digits)
